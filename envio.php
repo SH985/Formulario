702915edc5725b311b900cb6ec6945503c478ce4
SH985 <69173024+SH985@users.noreply.github.com>
2020-09-03 17:48:29 -0500

    Update envio.php

--- envio.php
+++ envio.php
@@ -12,8 +12,8 @@ $header .= "Content-Type: text/plain";
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+$message .= "Mensaje: " . $_POST["message"] . " \r\n";
+$message .= "Enviado el: " . date("d/m/Y", time());
 
 $para = "sebastianhenao.sandoval@gmail.com";
 $asunto = "Me interesa un Vehiculo";

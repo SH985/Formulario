d15ca9ea205556407ff04c31be684945deff95d5
SH985 <69173024+SH985@users.noreply.github.com>
2020-09-03 17:56:59 -0500

    Update envio.php

--- envio.php
+++ envio.php
@@ -5,15 +5,15 @@ $phone = $_POST[phone];
 $message = $_POST[message];
 
 $header = From:  . $mail .  \r\n;
-$header .= X-Mailer: PHP/ . phpversion() .  \r\n;
-$header .= Mime-Version: 1.0 \r\n;
-$header .= Content-Type: text/plain;
+$header = X-Mailer: PHP/ . phpversion() .  \r\n;
+$header = Mime-Version: 1.0 \r\n;
+$header = Content-Type: text/plain;
 
-$message = Este mensaje fue enviado por:  . $name .  \r\n;
-$message .= Su e-mail es:  . $mail .  \r\n";
-$message .= Teléfono de contacto:  . $phone .  \r\n;
-$message .= Mensaje:  . $_POST["message"] .  \r\n;
-$message .= Enviado el:  . date(d/m/Y, time());
+$message = Este mensaje fue enviado por:  . "$name" .  \r\n;
+$message = Su e-mail es:  . "$mail" .  \r\n;
+$message = Teléfono de contacto:  . "$phone" .  \r\n;
+$message = Mensaje:  . $_POST["message"] .  \r\n;
+$message = Enviado el:  . date(d/m/Y, time());
 
 $para = sebastianhenao.sandoval@gmail.com;
 $asunto = Me interesa un Vehiculo;
